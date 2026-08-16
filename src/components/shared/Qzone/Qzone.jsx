24be28dc9b2b22f39d5a0bf48290ad781fb37feb
rakib hasan <rakibwebdev78@gmail.com>
2026-08-16 16:07:21 +0600

    Q-Zone started.....

---
+++ src/components/shared/Qzone/Qzone.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Qzone = () => {
+    return (
+        <div className='w-full mt-6 py-6 bg-base-200'>
+            <h3 className='px-3'>Q-Zone</h3>
+        </div>
+    );
+};
+
+export default Qzone;
--- src/components/shared/RightAside/RightAside.jsx
+++ src/components/shared/RightAside/RightAside.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SocialButton from './../../common/SocialButton/SocialButton';
 import FindUs from './../FindUs/FindUs';
+import Qzone from './../Qzone/Qzone';
 
 const RightAside = () => {
     return (
@@ -8,6 +9,7 @@ const RightAside = () => {
             <h3 className='pb-3'>Login with</h3>
             <SocialButton />
             <FindUs />
+            <Qzone />
         </div>
     );
 };
